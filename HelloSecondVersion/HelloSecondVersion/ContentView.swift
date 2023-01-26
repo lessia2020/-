e299d4bd01ddf393c358268cff5eb6d0c53612c6
lessia2020 <63756124+lessia2020@users.noreply.github.com>
2023-01-26 11:38:57 +0100

    Add files via upload

---
+++ HelloSecondVersion/HelloSecondVersion/ContentView.swift
@@ -0,0 +1,79 @@
+//
+//  ContentView.swift
+//  HelloSecondVersion
+//
+//  Created by Olesia Nurislamova on 25.01.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var clicked = false
+    @State var greeting:String = "Hello "
+    @State var numberOfClicks = 0
+    
+    let myBgColor = Color(red: 255/255, green: 251/255, blue: 245/255)
+    let lightPurple = Color(red: 195/255, green: 172/255, blue: 208/255)
+    let darkPurple = Color(red: 48/255, green: 30/255 , blue: 103/255)
+        
+    
+    var body: some View {
+            
+        VStack {
+                
+                //Spacer()
+            
+                ZStack {
+                    Image(systemName: "heart")
+                        .imageScale(.large)
+                        .foregroundColor(darkPurple)
+                        .foregroundColor(.accentColor)
+                    VStack(spacing: 15) {
+                        Text(greeting)
+                        Text("Asado!")
+                    }
+                }
+                .foregroundColor(Color.white)
+                .font(.system(size: 110))
+                .background(lightPurple)
+                .padding()
+            
+                Spacer()
+                
+            
+            Button(action: {
+                numberOfClicks += 1
+                print(numberOfClicks)
+                if numberOfClicks  % 2 == 0 {
+                    greeting = "Hello"
+                } else {
+                    greeting = "Hi"
+                }
+            }, label: {
+                Text("Button")
+                    .font(.callout)
+                     .fontWeight(.heavy)
+                     .padding()
+                     .foregroundColor(darkPurple)
+                     .background(lightPurple)
+                
+            })
+            .padding(.top, 100.0)
+            
+            
+              
+            Spacer()
+               
+                
+                
+            }
+            myBgColor.ignoresSafeArea()
+        }
+    
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HelloSecondVersion/HelloSecondVersion/HelloSecondVersionApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloSecondVersionApp.swift
+//  HelloSecondVersion
+//
+//  Created by Olesia Nurislamova on 25.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct HelloSecondVersionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
