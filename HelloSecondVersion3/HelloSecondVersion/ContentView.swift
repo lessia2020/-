d23b86056a583e92d740ebe6fe6d409406108071
lessia2020 <63756124+lessia2020@users.noreply.github.com>
2023-01-31 15:32:33 +0100

    Add files via upload

---
+++ HelloSecondVersion3/HelloSecondVersion/ContentView.swift
@@ -0,0 +1,70 @@
+//
+//  ContentView.swift
+//  HelloSecondVersion
+//
+//  Created by Olesia Nurislamova on 25.01.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var greeting:String = "Hello"
+    //@State var numberOfClicks = 0
+    @State var isButtonClicked = false
+    
+    let myBgColor = Color(red: 255/255, green: 251/255, blue: 245/255)
+    let lightPurple = Color(red: 195/255, green: 172/255, blue: 208/255)
+    let darkPurple = Color(red: 48/255, green: 30/255 , blue: 103/255)
+    var body: some View {
+        VStack {
+                ZStack {
+                    Image(systemName: "heart")
+                        .imageScale(.large)
+                        .foregroundColor(darkPurple)
+                        .foregroundColor(.accentColor)
+                    VStack(spacing: 15) {
+                        //change(a: "Hi", b: "Hello")
+                        //Text(change())
+                        Text(greeting)
+                        Text("Asado")
+                        
+                    }
+                }
+                .foregroundColor(Color.white)
+                .font(.system(size: 110))
+                .background(lightPurple)
+                .padding()
+                Spacer()
+            Button(action: {
+                isButtonClicked.toggle()
+                change()
+            }, label: {
+                Text("Button")
+                    .font(.callout)
+                     .fontWeight(.heavy)
+                     .padding()
+                     .foregroundColor(darkPurple)
+                     .background(lightPurple)
+            })
+            .padding(.top, 100.0)
+            Spacer()
+            }
+            myBgColor.ignoresSafeArea()
+        }
+    
+    func change() -> String {
+        greeting = isButtonClicked ? "Hi" : "Hello"
+        return greeting
+    }
+    
+}
+    
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
